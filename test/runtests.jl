5ecdaa3e28da48bc2720b14ccc392992cec657b0
Rufat <rufat.ibrahimov.01@gmail.com>
2026-02-13 07:22:08 +0100

    removed check temporarily

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ using BenchmarkTools
 #problem = load_problem("blocks-problem.pddl", domain)
 # I ended up using the env-s from SymbolicPlanners akin to its tests, but I had to use PlanningDomains library.
 
-include("InitialSearch_check.jl")      #  Testing the initial mme search! functionality
+#include("InitialSearch_check.jl")      #  Testing the initial mme search! functionality
 #include("search_check.jl")      #  Testing the correct mme search! functionality (Doesn't hold rn, needs improvement of mme)
 include("Env1_comparison.jl")   #  blocksworld_axioms
 include("Env2_comparison.jl")   #  blocksworld
